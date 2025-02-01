d460fdcd60e47d253686092d26fdfe00705d1853
karthik-kolamuri <129858256+karthik-kolamuri@users.noreply.github.com>
2025-02-01 10:48:06 +0000

    JUST CRAETED THE SESSIONS

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotEnv = require("dotenv");
 const ejs = require('ejs')
-// const session = require('express-session')
-// const MongoDBStore = require('connect-mongodb-session')(session);
+const session = require('express-session')//session
+const MongoDBStore = require('connect-mongodb-session')(session);//actually we are passing a session as afunction to the MongoDBStore...
 const User = require('./models/User')
 // var bcrypt = require('bcryptjs');
 
@@ -19,8 +19,7 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }))
 
 
-
-mongoose.connect("mongodb+srv://karthikkolamur:Ka45h8k@udemyyy.l1ady.mongodb.net/shop?retryWrites=true&w=majority&appName=Udemyyy")
+mongoose.connect("mongodb+srv://karthikkolamur:Ka45h8k@udemyyy.l1ady.mongodb.net/session?retryWrites=true&w=majority&appName=Udemyyy")
     .then(() => {
         console.log("MongoDB Connected Succesfully!");
     })
@@ -28,7 +27,21 @@ mongoose.connect("mongodb+srv://karthikkolamur:Ka45h8k@udemyyy.l1ady.mongodb.net
         console.log(`${error}`);
     });
 
+    //creating a collection in the mongoDb 
+    const store=new MongoDBStore({
+        uri:"mongodb+srv://karthikkolamur:Ka45h8k@udemyyy.l1ady.mongodb.net/mysession?retryWrites=true&w=majority&appName=Udemyyy",
+        collection:"mysession2"
 
+    })
+
+
+    // creating a cookkie 
+app.use(session({
+    secret:"This is Karthik..",
+    resave:false,
+    saveUninitialized:true,
+    store:store
+}))
 
 
 app.get('/signup', (req, res) => {
@@ -41,6 +54,23 @@ app.get('/login', (req, res) => {
 
 app.get('/dashboard', (req, res) => {
     res.render('welcome')
+})
+
+app.post('/register',async(req,res,next)=>{
+    const {username,email,password}=req.body;
+    try{
+        const user=new User({
+            username,
+            email,
+            password
+        })
+        await user.save();
+        req.session.userDetails=user;
+        res.redirect('/login');
+    }
+    catch(err){
+        res.redirect('/signup')
+    }
 })
 
 app.listen(3000, () => {
