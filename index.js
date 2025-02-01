2bffbe8c4ec062af4e5a71e28f1f9c439c324840
karthik-kolamuri <129858256+karthik-kolamuri@users.noreply.github.com>
2025-02-01 09:35:32 +0000

    just removed all the code except the basic code

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotEnv = require("dotenv");
 const ejs = require('ejs')
-const session = require('express-session')
-const MongoDBStore = require('connect-mongodb-session')(session);
+// const session = require('express-session')
+// const MongoDBStore = require('connect-mongodb-session')(session);
 const User = require('./models/User')
-var bcrypt = require('bcryptjs');
+// var bcrypt = require('bcryptjs');
 
 
 const app = express();
@@ -28,24 +28,24 @@ mongoose.connect("mongodb+srv://karthikkolamur:Ka45h8k@udemyyy.l1ady.mongodb.net
         console.log(`${error}`);
     });
 
-const store = new MongoDBStore({
-    uri: process.env.MONGO_URI,
-    collection: "mySession"
-})
-app.use(session({
-    secret: "This is a secret",
-    resave: false,
-    saveUninitialized: true,
-    store: store
-}))
+// const store = new MongoDBStore({
+//     uri: process.env.MONGO_URI,
+//     collection: "mySession"
+// })
+// app.use(session({
+//     secret: "This is a secret",
+//     resave: false,
+//     saveUninitialized: true,
+//     store: store
+// }))
 
-const checkAuth = (req, res, next) => {
-    if (req.session.isAuthicated) {
-        next()
-    } else {
-        res.redirect('/signup')
-    }
-}
+// const checkAuth = (req, res, next) => {
+//     if (req.session.isAuthicated) {
+//         next()
+//     } else {
+//         res.redirect('/signup')
+//     }
+// }
 
 app.get('/signup', (req, res) => {
     res.render('register')
@@ -55,56 +55,56 @@ app.get('/login', (req, res) => {
     res.render('login')
 })
 
-app.get('/dashboard', checkAuth, (req, res) => {
+app.get('/dashboard', (req, res) => {
     res.render('welcome')
 })
 
-app.post('/register', async(req, res) => {
-    const { username, email, password } = req.body
+// app.post('/register', async(req, res) => {
+//     const { username, email, password } = req.body
 
-    let user = await User.findOne({ email })
-    if (user) {
-        return res.redirect('/signup')
-    }
-    const hashedPassword = await bcrypt.hash(password, 12)
+//     let user = await User.findOne({ email })
+//     if (user) {
+//         return res.redirect('/signup')
+//     }
+//     const hashedPassword = await bcrypt.hash(password, 12)
 
-    user = new User({
-        username,
-        email,
-        password: hashedPassword
-    })
-    req.session.person = user.username
-    await user.save()
-    res.redirect('/login')
+//     user = new User({
+//         username,
+//         email,
+//         password: hashedPassword
+//     })
+//     req.session.person = user.username
+//     await user.save()
+//     res.redirect('/login')
 
-})
+// })
 
-app.post('/user-login', async(req, res) => {
-    const { email, password } = req.body
+// app.post('/user-login', async(req, res) => {
+//     const { email, password } = req.body
 
-    const user = await User.findOne({ email })
+//     const user = await User.findOne({ email })
 
-    if (!user) {
-        return res.redirect('/signup')
-    }
+//     if (!user) {
+//         return res.redirect('/signup')
+//     }
 
-    const checkPassword = await bcrypt.compare(password, user.password)
+//     const checkPassword = await bcrypt.compare(password, user.password)
 
-    if (!checkPassword) {
-        return res.redirect('/signup')
-    }
-    req.session.isAuthicated = true
-    res.redirect('/dashboard')
+//     if (!checkPassword) {
+//         return res.redirect('/signup')
+//     }
+//     req.session.isAuthicated = true
+//     res.redirect('/dashboard')
 
-})
+// })
 
-app.post('/logout', (req, res) => {
-    req.session.destroy((err) => {
-        if (err) throw err;
-        res.redirect('/signup')
-    })
-})
+// app.post('/logout', (req, res) => {
+//     req.session.destroy((err) => {
+//         if (err) throw err;
+//         res.redirect('/signup')
+//     })
+// })
 
-app.listen(PORT, () => {
+app.listen(3000, () => {
     console.log(`Server started and Running @ ${PORT}`);
 });
