4f8d9c2f6ab267f264e3fab04fa8b9b83bf5de97
karthik-kolamuri <129858256+karthik-kolamuri@users.noreply.github.com>
2025-02-01 09:38:26 +0000

    removed the  code releated to the sessions

--- index.js
+++ index.js
@@ -28,24 +28,8 @@ mongoose.connect("mongodb+srv://karthikkolamur:Ka45h8k@udemyyy.l1ady.mongodb.net
         console.log(`${error}`);
     });
 
-// const store = new MongoDBStore({
-//     uri: process.env.MONGO_URI,
-//     collection: "mySession"
-// })
-// app.use(session({
-//     secret: "This is a secret",
-//     resave: false,
-//     saveUninitialized: true,
-//     store: store
-// }))
 
-// const checkAuth = (req, res, next) => {
-//     if (req.session.isAuthicated) {
-//         next()
-//     } else {
-//         res.redirect('/signup')
-//     }
-// }
+
 
 app.get('/signup', (req, res) => {
     res.render('register')
@@ -58,52 +42,6 @@ app.get('/login', (req, res) => {
 app.get('/dashboard', (req, res) => {
     res.render('welcome')
 })
-
-// app.post('/register', async(req, res) => {
-//     const { username, email, password } = req.body
-
-//     let user = await User.findOne({ email })
-//     if (user) {
-//         return res.redirect('/signup')
-//     }
-//     const hashedPassword = await bcrypt.hash(password, 12)
-
-//     user = new User({
-//         username,
-//         email,
-//         password: hashedPassword
-//     })
-//     req.session.person = user.username
-//     await user.save()
-//     res.redirect('/login')
-
-// })
-
-// app.post('/user-login', async(req, res) => {
-//     const { email, password } = req.body
-
-//     const user = await User.findOne({ email })
-
-//     if (!user) {
-//         return res.redirect('/signup')
-//     }
-
-//     const checkPassword = await bcrypt.compare(password, user.password)
-
-//     if (!checkPassword) {
-//         return res.redirect('/signup')
-//     }
-//     req.session.isAuthicated = true
-//     res.redirect('/dashboard')
-
-// })
-
-// app.post('/logout', (req, res) => {
-//     req.session.destroy((err) => {
-//         if (err) throw err;
-//         res.redirect('/signup')
-//     })
-// })
 
 app.listen(3000, () => {
     console.log(`Server started and Running @ ${PORT}`);
